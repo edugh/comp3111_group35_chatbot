a031b56d76c40bef88066ce48ec4f5d0b9701096
LeonTsangUST <kttsangaa@connect.ust.hk>
2017-11-02 23:59:05 +0800

    modified matching method

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -27,6 +27,9 @@ import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
 import java.util.function.BiConsumer;
 
+import java.util.ArrayList; //new
+import java.util.List; //new
+
 import com.example.bot.spring.model.FAQ;
 import com.example.bot.spring.model.Plan;
 import com.linecorp.bot.model.event.source.Source;
@@ -193,12 +196,46 @@ public class KitchenSinkController {
 
 	// TODO: Should we use some service like wit.ai for our milestone 3?
 	// or at least a better matching thing...
-	private boolean stupidFuzzyMatch(String question, String text) {
+	public double stringSimilarityScore(List<char[]> bigram1, List<char[]> bigram2) {
+	    List<char[]> copy = new ArrayList<char[]>(bigram2);
+	    int matches = 0;
+	    for (int i = bigram1.size(); --i >= 0;) {
+	        char[] bigram = bigram1.get(i);
+	        for (int j = copy.size(); --j >= 0;) {
+	            char[] toMatch = copy.get(j);
+	            if (bigram[0] == toMatch[0] && bigram[1] == toMatch[1]) {
+	                copy.remove(j);
+	                matches += 2;
+	                break;
+	            }
+	        }
+	    }
+	    return (double) matches / (bigram1.size() + bigram2.size());
+	}
+	
+	public static List<char[]> bigram(String input) {
+	    ArrayList<char[]> bigram = new ArrayList<char[]>();
+	    for (int i = 0; i < input.length() - 1; i++) {
+	        char[] chars = new char[2];
+	        chars[0] = input.charAt(i);
+	        chars[1] = input.charAt(i+1);
+	        bigram.add(chars);
+	    }
+	    return bigram;
+	}
+	
+	private boolean matchingQA(String question, String text) {
+		List<char[]> bigram1 = bigram(question);
+		List<char[]> bigram2 = bigram(text);
+		return (stringSimilarityScore(bigram1, bigram2) > 0.5);
+		
+		/** 
 		Set<String> targets = new HashSet<>(Arrays.asList(question.split(" ")));
 		String[] text_words = text.split(" ");
 		double num_words = (targets.size() + text_words.length) / 2.;
 		double matches = Arrays.stream(text_words).mapToInt(text_word -> targets.contains(text_word)? 1:0).sum();
-		return (matches / num_words) > 0.5;
+		return (matches / num_words) > 0.5; 
+		*/
 	}
 
 	/*
@@ -209,7 +246,7 @@ public class KitchenSinkController {
 	private List<Message> tryHandleFAQ(String text, Source source) {
 		ArrayList<FAQ> faqs = database.getFAQs();
 		for (FAQ faq: faqs) {
-			if (stupidFuzzyMatch(faq.question, text)) {
+			if (matchingQA(faq.question, text)) {
 				return Collections.singletonList(new TextMessage(faq.answer));
 			}
 		}
@@ -226,7 +263,7 @@ public class KitchenSinkController {
 
 	private List<Message> tryHandleTourSearch(String text, Source source) {
 		// TODO(Jason): match less idiotically, parse parameters
-		if (stupidFuzzyMatch("Which tours are available", text)) {
+		if (matchingQA("Which tours are available", text)) {
 			// TODO(Jason): real search
 			ArrayList<Plan> plans = database.getPlans();
 			if (plans.size() == 0) {
