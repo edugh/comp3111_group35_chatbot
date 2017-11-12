132169f0e5ab4d6f62fbcfb4386c4ff0d82c967d
jgzuke <jgzuke@gmail.com>
2017-11-13 02:43:30 +0800

    that was stupid

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/AIApiWrapper.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/AIApiWrapper.java
@@ -12,16 +12,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AIApiWrapper {
     private static final String API_KEY = "c4b93edcda0747c483f499c40f0814e1";
-    private AIConfiguration configuration = new AIConfiguration(API_KEY);
-    private AIDataService dataService = new AIDataService(configuration);
 
-    public Result getIntent(String text, Source source) {
+    public static Result getIntent(String text, Source source) {
         AIRequest request = new AIRequest(text);
-        //TODO(Jason): why does this not work...
+        AIConfiguration configuration = new AIConfiguration(API_KEY);
+        AIDataService dataService = new AIDataService(configuration, source::getUserId);
         request.setSessionId(source.getUserId());
         try {
             AIResponse response = dataService.request(request);
-            log.info("USER SESSION ID: {}", response.getSessionId());
             if (response.getStatus().getCode() == 200) {
                 return response.getResult();
             } else {
