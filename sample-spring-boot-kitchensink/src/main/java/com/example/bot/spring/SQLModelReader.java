c8afd2d8ea5def2afb619cbc4f6e1f136826e251
Jason Zukewich <jgzuke@gmail.com>
2017-11-20 22:41:44 +0800

    Document classes (#71)
    * some documentation
    
    * document test classes
    
    * document classes

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLModelReader.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLModelReader.java
@@ -2,6 +2,10 @@ package com.example.bot.spring;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Utility class to allow exceptions to be thrown when serializing our models
+ * @param <T>
+ */
 @FunctionalInterface
 public interface SQLModelReader<T> {
     T apply(ResultSet t) throws SQLException;
